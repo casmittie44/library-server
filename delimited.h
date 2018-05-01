71a33323b6935c21e5b925eda897d6ea8906805f
Caleb Smith <casmittie44@gmail.com>
2018-04-30 20:46:36 -0700

    Fixed error with DDSearch function. The issue: the use of the function GoToNextEntry in DDSearch was resulting in entries being skipped, since the use of ReadEntry would push the file buffer up to the next entry already. This resulted in an error in ReadEntry which resulted in a null return from DDSearch. This has been resolved. DDSearch is now operational. It does not really work for our application in server.c however, as the keyword entered by the user will have a trailing '\n' character. This will cause a mismatch, as DDSearch will be looking for an exact match in the database. This will need to be resolved.

--- delimited.h
+++ delimited.h
@@ -15,9 +15,20 @@ typedef struct {
 typedef struct {
    char* contents;
 } DDEntry;
+
+
 DD* DDCreateDatabase(const char* fileName, char delimiter);
 void DDFreeStructure(DD*);
+
+// Insert a new entry into the database
+// Returns 0 on success and -1 on failure
 int DDInsertEntry(DD* database, char* entry);
+
 int DDRemoveEntry(DD* database, char* entry, int len);
-char** DDSearch(DD* database, char* entry, int len);
+
+// Search database for entries containing the string keywordSearch, which has length len.
+// This function will return any entries containing the keyword in any of its fields
+// as a dynamically allocated char*
+// This structure will need to be freed after use
+char* DDSearch(DD* database, char* entry, int len);
 #endif
---
+++ hacking.c
@@ -0,0 +1,53 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Prints error message and forces program exit
+void fatal(char* message)
+{
+   char errorMessage[100];
+  
+   strcpy(errorMessage, "[!!] Fatal Error ");
+   strncat(errorMessage, message, 83);
+   perror(errorMessage);
+   exit(-1);
+}
+
+// Less error prone function to zero out memory
+void memzero(void* ptr, size_t size) {
+   memset(ptr, 0, size);
+}
+
+// Error checked malloc function
+void* ec_malloc(unsigned int size) {
+   void *ptr;
+   ptr = malloc(size);
+   if(ptr == NULL)
+      fatal("in ec_malloc() on memory allocation");
+   return ptr;
+}
+
+// Dumps raw memory in hex byte and printable split format
+void dump(const unsigned char *data_buffer, const unsigned int length) {
+   unsigned char byte;
+   unsigned int i,j;
+   for(i=0; i < length; i++) {
+      byte = data_buffer[i];
+      printf("%02x ", data_buffer[i]); // Display byte in hex
+
+      if(((i%16 == 15) || (i == length-1))) {
+	    for(j = 0; j < 15 - (i%16); j++)
+	       printf("    ");
+	    printf("| ");
+	    for(j =(i-(i%16)); j < i; j++) {
+	       byte = data_buffer[j];
+	       if((byte > 31) && (byte < 127))
+		  printf("%c", byte);
+	       else
+		  printf(".");
+	    }
+
+	    printf("\n"); // End of dump line (each line is 16 bytes)
+      }
+   }
+}
